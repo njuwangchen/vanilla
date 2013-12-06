f0c279c9df200b0fccf3b1a4582ed3d7b646ff26
Haoyu Yang <1402157494@qq.com>
2013-12-06 19:21:53 +0800

    xxx

--- Vanilla/Vanilla/VanillaAppDelegate.h
+++ Vanilla/Vanilla/VanillaAppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by 王晨Clark on 13-12-6.
 //  Copyright (c) 2013年 王晨Clark. All rights reserved.
 //
-
+//
 #import <UIKit/UIKit.h>
 
 @interface VanillaAppDelegate : UIResponder <UIApplicationDelegate>
